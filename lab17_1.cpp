758fd58229f59efc9fd5eced5a6ba2555dc453c2
lab302-32 <lab302-32@labeng.local>
2020-03-10 16:47:52 +0700

    2

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -2,71 +2,105 @@
 #include<string>
 #include<fstream>
 #include<vector>
-#include<cstring>
 using namespace std;
-
-int main(){
-    ifstream s("name_score.txt");
-    string ltxt;
-    int i=0;
-    char format[] = "%[^:]: %d %d %d";
-    vector<string> name;
+int main() {
+  ifstream f;
+  f.open("name_score.txt");
+    string cs;
+    int n=0;
+      while(getline(f,cs)){
+        n++;
+      }
+  f.close();
+  f.open("name_score.txt");
+    vector<int> sum;
     vector<string> grade;
-    vector<int> score;
-    int t1,t2,t3;
-    char n[100]={};
-    while(getline(s,ltxt)){
+    vector<string> namev;
+    vector<string> cap;
+    char name[100];
+    string N,g;
+    char format[] ="%[^:]: %d %d %d";
+    int i=0,a,b,c;
+      while(getline(f,cs)){
+        const char * text = cs.c_str();
+        sscanf(text,format,name,&a,&b,&c);
+        N=name;
 
-        sscanf(ltxt.c_str(),format,n,&t1,&t2,&t3);
-        name.push_back(n);
-        score.push_back(t1+t2+t3);
-        if(score.at(i)>=80)grade.push_back("A");
-        else if(score.at(i)>=70)grade.push_back("B");
-        else if(score.at(i)>=60)grade.push_back("C");
-        else if(score.at(i)>=50)grade.push_back("D");
-        else grade.push_back("F");
+        namev.push_back(N);
+        if((a+b+c)>=80)g="A";
+        else if((a+b+c)>=70)g="B";
+        else if((a+b+c)>=60)g="C";
+        else if((a+b+c)>=50)g="D";
+        else g="F";
+        grade.push_back(g);
+        sum.push_back(a+b+c);
         i++;
+      }
+    f.close();
+    i=0;
+    string order;
+    cout<<"Please input your command :";
+    getline(cin,order);
 
+    for(int i=0;i<order.size();i++){
+      order[i]=tolower(order[i]);
     }
-    char typ[10]={},doftyp[10]={};
-    
-    do{
-        cout<<"Please input your command : ";
-        cin >>typ;
-        for(int j=0;j<strlen(typ);j++){typ[j]=toupper(typ[j]);}
-        if(strcmp(typ,"EXIT")==0)break;
-        
-        if(strcmp(typ,"GRADE")==0){
-            cin >> doftyp;
-            for(int j=0;j<strlen(doftyp);j++){
-                doftyp[j]=toupper(doftyp[j]);
+
+    order+=":";
+    char pformat[] ="%s %[^:]";
+        while(order!="exit:"){
+          string sst="Invalid",snd="";
+          const char * sorder = order.c_str();
+          char st[100],nd[100];
+          sscanf(sorder,pformat,st,nd);
+          sst=st;
+          snd=nd;
+          snd[0]=toupper(snd[0]);
+          for(int i=1;i<snd.size();i++){
+            if(snd[i]==' '){
+              snd[i+1]=toupper(snd[i+1]);
+              i++;
             }
-            int c=0;
-            for(int j=0;j<i;j++){
-                if(grade.at(j)==doftyp){
-                    cout<<name.at(j)<<endl;
-                    c=1;
+            else{snd[i]=tolower(snd[i]);}
+          }
+
+            if(sst=="name"){
+              int check=0;
+              for(int i=0;i<n;i++){
+                if(snd==namev[i]){
+                  cout<<namev[i]<<"'s grade = "<<grade[i]<<endl;
+                  check=1;
+                  
                 }
+              }
+              if(check==0)cout<<"Cannot found"<<endl;
             }
-            if(c==0)cout<<"not found\n";
-        }else if(strcmp(typ,"NAME")==0){
-            cin >> doftyp;
-            for(int j=0;j<strlen(doftyp);j++){
-                doftyp[j]=toupper(doftyp[j]);
-            }
-            int c=0;
-            for(int j=0;j<i;j++){
-                string ntmp=name.at(j);
-                for(int k=0;k<ntmp.size();k++){
-                    ntmp[k]=toupper(ntmp[k]);
+
+            else if(sst=="grade"){
+              int check=0;
+              for(int i=0;i<n;i++){
+                if(snd==grade[i]){
+                  cout<<namev[i]<<endl;
+                  check=1;
                 }
-               
-                if(strcmp(ntmp.c_str(),doftyp)==0){
-                    cout<<name.at(j)<< " grade = "<<grade.at(j)<<endl;
-                    c=1;
-                }
+              }
+              if(check==0) cout<<"Cannot found"<<endl;
             }
-            if(c==0)cout<<"not found\n";
-        }else cout<<"Invalid command\n";
-    }while(true);
+
+            else{
+              cout<<"Invalid command"<<endl;
+            }
+            cout<<"Please input your command :";
+            getline(cin,order);
+            
+            /*cout<<order;*/
+            for(int i=0;i<order.size();i++){
+              order[i]=tolower(order[i]);
+              
+            }
+            order+=":";
+           
+        }
+ 
+  return 0;
 }
